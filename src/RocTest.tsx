5aaf2832db785cc212302c7fb5af5503b8fb7378
Caleb Jacob <thecalebjacob@gmail.com>
2024-04-25 11:55:59 -0600

    Adding example of reading url query param

--- src/RocTest.tsx
+++ src/RocTest.tsx
@@ -1,10 +1,18 @@
 import RocChildTest from "./RocChildTest";
 import s from "./RocTest.module.css";
 
-function RocTest() {
+type Props = {
+  foobar?: string; // An example of reading an injected URL query param
+};
+
+function RocTest({ foobar }: Props) {
   return (
     <div className={s.wrapper}>
       <h1>This is a test</h1>
+      <p>
+        Here is the URL query param value of foobar:{" "}
+        <b>{typeof foobar === "undefined" ? "missing (undefined)" : foobar === "" ? "empty string" : foobar}</b>
+      </p>
       <RocChildTest message="hello world!" />
     </div>
   );
